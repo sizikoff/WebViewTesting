593840b407d67187932cd04a71aee70dfab8baf2
Вовчик <sizikoffps@gmail.com>
2025-05-31 14:41:57 +0300

    Я добавил что то в клаасс Somewhere.java

--- app/src/main/java/com/amicus/myapplication/Somewhere.java
+++ app/src/main/java/com/amicus/myapplication/Somewhere.java
@@ -4,4 +4,9 @@ package com.amicus.myapplication;
  * Created by sizik on 31.05.2025.
  */
 public class Somewhere {
+    String name;
+
+    public Somewhere(String name) {
+        this.name = name;
+    }
 }
